7a3152d3aa76aa30b56a1dabc1d9401b48858cf7
Joel Grant <joelmgrant@me.com>
2022-03-27 17:35:56 -0600

    Update Poro

--- app/poros/photo.rb
+++ app/poros/photo.rb
@@ -1,4 +1,9 @@
 class Photo
-  def initialize
+  attr_reader :id, :photo_url, :photographer, :photographer_url
+  def initialize(data)
+    @id = nil
+    @photo_url = data[:photos][0][:url]
+    @photographer = data[:photos][0][:photographer]
+    @photographer_url = data[:photos][0][:photographer_url]
   end
 end
